f932c209d5dfc2863dd637905acf4df00664b54a
CieloAngel <cieloangel@cox.net>
2017-06-23 22:06:28 -0500

    Just added a few comments and updated mcmod.info before starting major changes

--- src/main/java/net/cieloangel/testmod/TestMod.java
+++ src/main/java/net/cieloangel/testmod/TestMod.java
@@ -11,18 +11,25 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+
+// @Mod marks this class as the main mod class so Forge knows to load from here.
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 
 public class TestMod {
 	
+	// @SidedProxy allows Forge to detect which side our mod is running on and injects the correct
+	// proxy into the proxy field
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY, serverSide = Reference.SERVER_PROXY)
 	public static CommonProxy proxy;
 	
+	// @Instance lets Forge inject the instance of our mod that is used into it.
+	// Important especially when working with GUI's
 	@Instance
 	public static TestMod modInstance;
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
+		System.out.println(Reference.MOD_NAME + " is loading...");
 		ModBlocks.init();
 		ModBlocks.register();
 		
